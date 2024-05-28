af83290906fd79136f24551e2f0fe3e507bad0e5
Ayelechyilma <ayelech63@gmail.com>
2024-05-28 12:29:13 -0700

    added redirect file

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-
-import { useContext, useEffect } from 'react';
-import './App.css';
-import Routing from './Router';
-import { DataContext } from './components/DataProvider/DataProvider';
-import { auth } from './Utility/firebase';
-import { Type } from './Utility/action.type';
-
+import { useContext, useEffect } from "react";
+import "./App.css";
+import Routing from "./Router";
+import { DataContext } from "./components/DataProvider/DataProvider";
+import { auth } from "./Utility/firebase";
+import { Type } from "./Utility/action.type";
 
 function App() {
-  const[{user}, dispatch] =useContext(DataContext)
+	const [{ user }, dispatch] = useContext(DataContext);
 
-
-
-  useEffect(()=>{
-    auth.onAuthStateChanged((authUser)=>{
-      if (authUser){
-        dispatch({
-          type: Type.SET_USER,
-          user:authUser
-        })
-      } else{
-        dispatch({
-          type: Type.SET_USER,
-          user:null
-        })
-      }
-    })
-  },[])
-  return (
-    <div className="App">
-      <Routing/>
-
-    </div>
-  );
+	useEffect(() => {
+		auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				dispatch({
+					type: Type.SET_USER,
+					user: authUser,
+				});
+			} else {
+				dispatch({
+					type: Type.SET_USER,
+					user: null,
+				});
+			}
+		});
+	}, [user]);
+	return (
+		<div className="App">
+			<Routing />
+		</div>
+	);
 }
 
 export default App;
